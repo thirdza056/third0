4514bd28ffcb0ad05b716a1bdda0473a6422436b
thirdza056 <33078689+thirdza056@users.noreply.github.com>
2017-12-11 12:20:20 +0700

    Talk.py

--- LINETCR/Api/Talk.py
+++ LINETCR/Api/Talk.py
@@ -90,7 +90,7 @@ class Talk:
     self.transport.path = self.auth_query_path
 
     qr = self.client.getAuthQrcode(True, "Tyfe's Operation")
-    callback("Copy to Line and Click\nYour ??????? QR is: line://au/q/" + qr.verifier + "\nCreator By?????????????")
+    callback("Copy to Line and Click\nYour จัดไป QR is: line://au/q/" + qr.verifier + "\nCreator Byกูก็อปเค้ามา")
 
     r = requests.get("https://" + self.host + self.wait_for_mobile_path, headers={
       "X-Line-Application": self.LA,
